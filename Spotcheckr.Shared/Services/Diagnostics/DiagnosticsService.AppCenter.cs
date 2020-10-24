6e44c600e0748ad00940b3ba082b397b64934755
Nitish Sachar <nitish.sachar@protonmail.com>
2020-10-24 09:10:04 -0700

    add services folder

---
+++ Spotcheckr.Shared/Services/Diagnostics/DiagnosticsService.AppCenter.cs
@@ -0,0 +1,14 @@
+﻿#if __IOS__ || __ANDROID__ || NETFX_CORE || __MACOS__
+using System;
+using System.Collections.Generic;
+using Microsoft.AppCenter.Crashes;
+
+namespace Spotcheckr.Shared.Services
+{
+    public static partial class DiagnosticsService
+    {
+	    static partial void TrackErrorPartial(Exception exception, Dictionary<string, string> properties = null) =>
+		    Crashes.TrackError(exception, properties);
+    }
+}
+#endif
---
+++ Spotcheckr.Shared/Services/Diagnostics/DiagnosticsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spotcheckr.Shared.Services
+{
+    public static partial class DiagnosticsService
+    {
+	    public static void TrackError(Exception exception, Dictionary<string, string> properties = null) =>
+		    TrackErrorPartial(exception, properties);
+
+	    static partial void TrackErrorPartial(Exception exception, Dictionary<string, string> properties = null);
+    }
+}
