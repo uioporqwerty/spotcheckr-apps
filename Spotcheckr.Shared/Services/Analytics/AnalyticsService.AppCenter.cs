59f7f89a2738e7f3ba371732f77f44f066361751
Nitish Sachar <nitish.sachar@protonmail.com>
2020-10-24 23:28:12 -0700

    remove unsupported appcenter macos configuration

--- Spotcheckr.Shared/Services/Analytics/AnalyticsService.AppCenter.cs
+++ Spotcheckr.Shared/Services/Analytics/AnalyticsService.AppCenter.cs
@@ -11,10 +11,9 @@ namespace Spotcheckr.Shared.Services
 		{
 			const string iOSId = "5ff920d3-d043-43b1-afc1-3267cf12beb6";
 			const string androidId = "51904ea9-5554-4ccc-aaae-c1d7285e118d";
-			const string macOSId = "b8459abe-d1cb-40a2-bf56-a36041fcac06";
 			const string uwpId = "98d917fa-1080-4d19-b46f-a1eed12b0c09";
 
-			AppCenter.Start($"ios={iOSId};uwp={uwpId};macos={macOSId};android={androidId}",
+			AppCenter.Start($"ios={iOSId};uwp={uwpId};android={androidId}",
 				typeof(Analytics), typeof(Crashes));
 		}
 
